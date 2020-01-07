dc36956dfe8d7249d72aef93c889dc72b28d6dec
Andrea Bottini <andrea.bottini22@gmail.com>
2020-01-07 16:26:51 +0100

    Impostazione esercizio Mail

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,1 +1,4 @@
-alert('sta funzionando?')
+//alert('sta funzionando?')
+
+var emailInserite = ["luca@gmail.com", "marco.bianchi@gmail.com", "andrea.rossi@gmail.com", "matteo.gialli@gmail.com" ];
+console.log(emailInserite);
