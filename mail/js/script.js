efcf3c1f663e58211bf097eaf2d1adbfe9d410d5
Andrea Bottini <andrea.bottini22@gmail.com>
2020-01-07 16:48:22 +0100

    Prova inserimento array e push ulteriore mail

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,4 +1,12 @@
 //alert('sta funzionando?')
 
-var emailInserite = ["luca@gmail.com", "marco.bianchi@gmail.com", "andrea.rossi@gmail.com", "matteo.gialli@gmail.com" ];
+var emailInserite = ["luca.verdi@gmail.com", "marco.bianchi@gmail.com", "andrea.rossi@gmail.com", "matteo.gialli@gmail.com" ];
+//console.log(emailInserite);
+
+for (var i = 0; i < emailInserite.length; i++) {
+  console.log(emailInserite[i]);
+}
+
+emailInserite.push ("franco.arancioni@gmail.com");
+
 console.log(emailInserite);
